5292d05369ab6a8fa2ab5e7c94cba622e80cf3c9
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-18 23:51:03 -0300

    Se modifico la clase base de PacmanLevelScene

--- src/pacman/scene/PacmanLevelScene.java
+++ src/pacman/scene/PacmanLevelScene.java
@@ -1,13 +1,21 @@
 package pacman.scene;
 
-import com.uqbar.vainilla.GameScene;
+import com.uqbar.vainilla.GraphGameScene;
 
-public class PacmanLevelScene extends GameScene {
+public class PacmanLevelScene extends GraphGameScene {
+
+	
+	
+	public PacmanLevelScene(String map) {
+		super(map);
+	}
 
 	@Override
 	protected void initializeComponents() {
 		// TODO Auto-generated method stub
 
 	}
+
+
 
 }
