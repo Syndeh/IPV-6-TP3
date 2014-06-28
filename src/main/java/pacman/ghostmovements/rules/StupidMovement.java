69aff7b41fd91d97ae8f9a1d8ddcce64048f7f96
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-28 20:18:48 -0300

    Se mejoro la logica del StupidMovement, agregando una eleccion aleatoria del camino

--- src/main/java/pacman/ghostmovements/rules/StupidMovement.java
+++ src/main/java/pacman/ghostmovements/rules/StupidMovement.java
@@ -17,62 +17,50 @@ public class StupidMovement implements Movement {
 		MapGraph<Valuable> mapGraph = ghost.getScene().getMapGraph();
 		Node<Valuable> currentNode = mapGraph.obtainNode((int)ghost.getY()/2,(int)ghost.getX()/2);
 		if(ghost.getDirection().equals(DIRECTION_RIGHT)){
-			if(currentNode.isHasRightAdjacency()){
+			if(currentNode.isHasRightAdjacency() && currentNode.getAdjancencies().size()<=2){
 				ghost.setX(currentNode.getRightAdjacency().getColumn()* 2);
 				ghost.setY(currentNode.getRightAdjacency().getRow()* 2);
-			}else if(currentNode.isHasDownAdjacency()){
-				ghost.setX(currentNode.getDownAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getDownAdjacency().getRow()* 2);
-			}else if(currentNode.isHasLeftAdjacency()){
-				ghost.setX(currentNode.getLeftAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getLeftAdjacency().getRow()* 2);
-			}else {
-				ghost.setX(currentNode.getUpAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getUpAdjacency().getRow()* 2);
+			}else{
+				Node<Valuable> nextAdjacency = this.getNextAdjacency(currentNode, currentNode.getRightAdjacency());
+				ghost.setX(nextAdjacency.getColumn() * 2);
+				ghost.setY(nextAdjacency.getRow()* 2);
 			}
-		}else if(ghost.getDirection().equals(DIRECTION_DOWN)){
+		}else if(ghost.getDirection().equals(DIRECTION_DOWN) && currentNode.getAdjancencies().size()<=2){
 			if(currentNode.isHasDownAdjacency()){
 				ghost.setX(currentNode.getDownAdjacency().getColumn()* 2);
 				ghost.setY(currentNode.getDownAdjacency().getRow()* 2);
-			}else if(currentNode.isHasLeftAdjacency()){
-				ghost.setX(currentNode.getLeftAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getLeftAdjacency().getRow()* 2);
-			}else if(currentNode.isHasUpAdjacency()){
-				ghost.setX(currentNode.getUpAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getUpAdjacency().getRow()* 2);
 			}else{
-				ghost.setX(currentNode.getRightAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getRightAdjacency().getRow()* 2);
+				Node<Valuable> nextAdjacency = this.getNextAdjacency(currentNode, currentNode.getDownAdjacency());
+				ghost.setX(nextAdjacency.getColumn() * 2);
+				ghost.setY(nextAdjacency.getRow()* 2);
 			}
-		}else if(ghost.getDirection().equals(DIRECTION_LEFT)){
+		}else if(ghost.getDirection().equals(DIRECTION_LEFT) && currentNode.getAdjancencies().size()<=2){
 			if(currentNode.isHasLeftAdjacency()){
 				ghost.setX(currentNode.getLeftAdjacency().getColumn()* 2);
 				ghost.setY(currentNode.getLeftAdjacency().getRow()* 2);
-			}else if(currentNode.isHasUpAdjacency()){
-				ghost.setX(currentNode.getUpAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getUpAdjacency().getRow()* 2);
-			}else if(currentNode.isHasRightAdjacency()){
-				ghost.setX(currentNode.getRightAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getRightAdjacency().getRow()* 2);
-			}if(currentNode.isHasDownAdjacency()){
-				ghost.setX(currentNode.getDownAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getDownAdjacency().getRow()* 2);
+			}else {
+				Node<Valuable> nextAdjacency = this.getNextAdjacency(currentNode, currentNode.getLeftAdjacency());
+				ghost.setX(nextAdjacency.getColumn() * 2);
+				ghost.setY(nextAdjacency.getRow()* 2);
 			}
 		}else{
-			if(currentNode.isHasUpAdjacency()){
+			if(currentNode.isHasUpAdjacency() && currentNode.getAdjancencies().size()<=2){
 				ghost.setX(currentNode.getUpAdjacency().getColumn() * 2);
 				ghost.setY(currentNode.getUpAdjacency().getRow()* 2);
-			}else if(currentNode.isHasRightAdjacency()){
-				ghost.setX(currentNode.getRightAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getRightAdjacency().getRow()* 2);
-			}else if(currentNode.isHasDownAdjacency()){
-				ghost.setX(currentNode.getDownAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getDownAdjacency().getRow()* 2);
 			}else{
-				ghost.setX(currentNode.getLeftAdjacency().getColumn()* 2);
-				ghost.setY(currentNode.getLeftAdjacency().getRow()* 2);
-			} 
+				Node<Valuable> nextAdjacency = this.getNextAdjacency(currentNode, currentNode.getUpAdjacency());
+				ghost.setX(nextAdjacency.getColumn() * 2);
+				ghost.setY(nextAdjacency.getRow()* 2);
+			}
+			
 		}
+		
+	}
+	private Node<Valuable> getNextAdjacency(Node<Valuable> currentNode,
+			Node<Valuable> currentAdjacency) {
+		int index = (int) (Math.random() * (currentNode.getAdjancencies().size()));
+		Node<Valuable> nextAdjacency = currentNode.getAdjancencies().get(index).getDestination();
+		return nextAdjacency;
 		
 	}
 }
