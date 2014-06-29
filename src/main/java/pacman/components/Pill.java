8c458e7483c9cc804033ebc58f566a7c2e2cdd52
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-28 21:23:02 -0300

    Se modifico Pill, para que no use un sprite

--- src/main/java/pacman/components/Pill.java
+++ src/main/java/pacman/components/Pill.java
@@ -1,14 +1,18 @@
 package pacman.components;
 
+import java.awt.Color;
+
 import pacman.scene.PacmanLevelScene;
 
 import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.appearances.Circle;
 import com.uqbar.vainilla.appearances.Sprite;
 
 public class Pill extends GameComponent<PacmanLevelScene> {
 
 	public Pill() {
-		this.setAppearance(Sprite.fromImage("images/pacmansprite.png").crop(43, 3, 13, 13).scaleTo(10, 10));
+		//this.setAppearance(Sprite.fromImage("images/pacmansprite.png").crop(43, 3, 13, 13).scaleTo(5, 5));
+		this.setAppearance(new Circle(Color.YELLOW, 5));
 	}
 	
 }
