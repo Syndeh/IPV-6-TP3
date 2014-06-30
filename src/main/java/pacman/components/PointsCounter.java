6873a2697bac48a4d9a0bbcefca2ebb00ff19dda
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-29 21:27:29 -0300

    Se eliminaron los harcodeos de PointsCounter

--- src/main/java/pacman/components/PointsCounter.java
+++ src/main/java/pacman/components/PointsCounter.java
@@ -7,15 +7,17 @@ import com.uqbar.vainilla.DeltaState;
 import com.uqbar.vainilla.GameScene;
 import com.uqbar.vainilla.appearances.Label;
 import com.uqbar.vainilla.components.AbstractCounter;
+import com.uqbar.vainilla.utils.ResourceUtil;
 
 public class PointsCounter extends AbstractCounter<GameScene> {
 	
-	private int points;
+	private int points=0;
+	private static int FONTSIZE = ResourceUtil.getResourceInt("PointsCounter.FONT.SIZE");
+	private static int Y = ResourceUtil.getResourceInt("PointsCounter.Y");
 	
 	public PointsCounter() {
-		this.points = 0;
-		this.setAppearance(new Label(new Font(Font.SANS_SERIF, Font.BOLD, 20), Color.YELLOW, "Puntos: " + this.getPoints()));
-		this.setY(490);
+		this.setAppearance(new Label(new Font(Font.SANS_SERIF, Font.BOLD, FONTSIZE), Color.YELLOW, "Puntos: " + this.getPoints()));
+		this.setY(Y);
 	}
 
 	@Override
