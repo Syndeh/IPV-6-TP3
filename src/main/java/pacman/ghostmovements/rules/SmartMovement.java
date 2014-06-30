f8e6ff258ad78816daae749cd974964ebdaab21e
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-29 21:35:51 -0300

    Se eliminaron los harcodeos de SmartMovement

--- src/main/java/pacman/ghostmovements/rules/SmartMovement.java
+++ src/main/java/pacman/ghostmovements/rules/SmartMovement.java
@@ -7,21 +7,22 @@ import com.uqbar.vainilla.graphs.Node;
 import com.uqbar.vainilla.graphs.Valuable;
 
 import pacman.components.Ghost;
+import pacman.utils.GlobalResources;
 
 public class SmartMovement implements Movement{
 
 	@Override
 	public void move(Ghost ghost) {
 		MapGraph<Valuable> mapGraph = ghost.getScene().getMapGraph();
-		Node<Valuable> source = mapGraph.obtainNode((int)ghost.getY()/2,(int)ghost.getX()/2);
+		Node<Valuable> source = mapGraph.obtainNode((int)ghost.getY()/GlobalResources.SCALEFACTOR,(int)ghost.getX()/GlobalResources.SCALEFACTOR);
 		Node<Valuable> destination = ghost.getScene().getPacmanPosition();
 		List<Node<Valuable>> path = mapGraph.getShortestPath(source, destination);
 		if(path!=null && path.size()>0)
 		{
 			source.setMinDistance(Double.POSITIVE_INFINITY);
 			source.setPrevious(null);
-			ghost.setX(path.get(0).getColumn() * 2);
-			ghost.setY(path.get(0).getRow()*2);
+			ghost.setX(path.get(0).getColumn() * GlobalResources.SCALEFACTOR);
+			ghost.setY(path.get(0).getRow()*GlobalResources.SCALEFACTOR);
 			path.get(0).setMinDistance(Double.POSITIVE_INFINITY);
 			path.get(0).setPrevious(null);
 		}
