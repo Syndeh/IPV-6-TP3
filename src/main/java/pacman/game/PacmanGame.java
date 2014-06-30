5cf797a98bb95e5d1b71613c80e5d603796b16ab
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-29 21:33:03 -0300

    Se eliminaron los harcodeos de PacmanGame

--- src/main/java/pacman/game/PacmanGame.java
+++ src/main/java/pacman/game/PacmanGame.java
@@ -2,14 +2,19 @@ package pacman.game;
 
 import java.awt.Dimension;
 
-
 import pacman.scene.PacmanLevelScene;
 
 import com.uqbar.vainilla.DesktopGameLauncher;
 import com.uqbar.vainilla.Game;
+import com.uqbar.vainilla.utils.ResourceUtil;
 
 public class PacmanGame extends Game {
 
+	private final static String MAP = ResourceUtil.getResourceString("PacmanGame.MAP");
+	private final static int WIDHT = ResourceUtil.getResourceInt("PacmanGame.WIDHT");
+	private final static int HEIGHT = ResourceUtil.getResourceInt("PacmanGame.HEIGHT");
+	private final static String TITLE = ResourceUtil.getResourceString("PacmanGame.TITLE"); 
+	
 	public static void main(String[] args) throws Exception {
 		new DesktopGameLauncher(new PacmanGame()).launch();
 	}
@@ -26,20 +31,20 @@ public class PacmanGame extends Game {
 
 	@Override
 	protected void setUpScenes() {
-		PacmanLevelScene scene = new PacmanLevelScene("mapa1.png");
+		PacmanLevelScene scene = new PacmanLevelScene(MAP);
 		this.setCurrentScene(scene);
 
 	}
 
 	@Override
 	public Dimension getDisplaySize() {
-		return new Dimension(460, 510);
+		return new Dimension(WIDHT, HEIGHT);
 
 	}
 
 	@Override
 	public String getTitle() {
-		return "Pacman";
+		return TITLE;
 	}
 
 }
